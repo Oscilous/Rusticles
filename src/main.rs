64a34005597fd98a6af3637edd26ce5acb36b544
Oscilous <artis.fils@gmail.com>
2023-06-30 21:29:44 +0200

    Close to finishing

--- src/main.rs
+++ src/main.rs
@@ -3,26 +3,49 @@ extern crate minifb;
 use minifb::{Key, MouseButton, MouseMode, Scale, Window, WindowOptions};
 mod physics;
 
-struct Screen{
+struct Screen {
     WIDTH: usize,
     HEIGHT: usize,
-    buffer: Vec<u32>
+    buffer: Vec<u32>,
+    window: Window,
 }
 
-impl Screen{
+impl Screen {
     fn new(WIDTH: usize, HEIGHT: usize) -> Screen {
         let buffer: Vec<u32> = vec![0; WIDTH * HEIGHT]; // Calculate the third value
-
+        let mut window = Window::new(
+            "Pixel Renderer",
+            WIDTH,
+            HEIGHT,
+            WindowOptions {
+                scale: Scale::X4, // Scale factor
+                ..WindowOptions::default()
+            },
+        )
+        .expect("Unable to create window");
+        //Set FPS to ~60
+        window.limit_update_rate(Some(std::time::Duration::from_micros(16600)));
+        // Create a buffer to hold the pixel data
         Screen {
             WIDTH,
             HEIGHT,
             buffer,
+            window,
         }
     }
 }
-struct Particle{
-    const SAND_COLOR: u32 = 0xe4bc80;
-    const BACKGROUND_COLOR: u32 = 0x000000;
+enum Particle {
+    Sand,
+    Background,
+}
+
+impl Particle {
+    fn get_color(&self) -> u32 {
+        match *self {
+            Particle::Sand => 0xe4bc80,
+            Particle::Background => 0x000000,
+        }
+    }
 }
 enum Position {
     RightSide,
@@ -32,58 +55,44 @@ enum Position {
 
 fn main() {
     // Create a window with a specific resolution
-    let Screen: Screen = Screen::new(200, 100);
-    let mut window = Window::new(
-        "Pixel Renderer",
-        Screen.WIDTH,
-        Screen.HEIGHT,
-        WindowOptions {
-            scale: Scale::X4, // Scale factor
-            ..WindowOptions::default()
-        },
-    )
-    .expect("Unable to create window");
-    //Set FPS to ~60
-    window.limit_update_rate(Some(std::time::Duration::from_micros(16600)));
-    // Create a buffer to hold the pixel data
-    window.HEIGHT
-    while window.is_open() && !window.is_key_down(Key::Escape) {
+    let mut Screen: Screen = Screen::new(200, 100);
+    while Screen.window.is_open() && !Screen.window.is_key_down(Key::Escape) {
         // Get the current mouse position
-        if window.get_mouse_down(MouseButton::Left) {
-            let mouse_pos: (f32, f32) = window.get_mouse_pos(MouseMode::Clamp).unwrap();
+        if Screen.window.get_mouse_down(MouseButton::Left) {
+            let mouse_pos: (f32, f32) = Screen.window.get_mouse_pos(MouseMode::Clamp).unwrap();
             // Get the coordinates of the mouse click
             let click_x: usize = mouse_pos.0 as usize;
             let click_y: usize = mouse_pos.1 as usize;
 
             // Set the pixel at the mouse click position to green
-            buffer[click_x + click_y * WIDTH] = SAND_COLOR; // RGB value for green
+            Screen.buffer[click_x + click_y * Screen.WIDTH] = Particle::Sand.get_color();
+            // RGB value for green
         }
 
-        update_physics(&mut buffer);
+        update_physics(&mut Screen);
 
         // Render the buffer to the window
-        window.update_with_buffer(&buffer, WIDTH, HEIGHT).unwrap();
+        Screen
+            .window
+            .update_with_buffer(&Screen.buffer, Screen.WIDTH, Screen.HEIGHT)
+            .unwrap();
     }
 }
 
-fn update_physics(buffer: &mut Vec<u32>) {
-    for y in (0..(HEIGHT - 1)).rev() {
-        for x in (0..(WIDTH - 1)).rev() {
+fn update_physics(Screen: &mut Screen) {
+    for y in (0..(Screen.HEIGHT - 1)).rev() {
+        for x in (0..(Screen.WIDTH - 1)).rev() {
             //Firstly sand physics
-            if buffer[x + y * WIDTH] == SAND_COLOR {
+            if Screen.buffer[x + y * Screen.WIDTH] == Particle::Sand.get_color() {
                 //If nothing is under, then gravity
-                match buffer[x + (y + 1) * WIDTH] {
+                match Screen.buffer[x + (y + 1) * Screen.WIDTH] {
                     //If underneath a sand particle is nothing
-                    BACKGROUND_COLOR => physics::gravity(buffer, x, y),
+                    BACKGROUND_COLOR => physics::gravity(Screen, x, y),
                     //If underneath a sand particle is another sand particle
-                    SAND_COLOR => match check_for_cases(x, y) {
-                        Position::RightSide => {
-                            physics::right_corner_stacking(buffer, x, y, SAND_COLOR)
-                        }
-                        Position::LeftSide => {
-                            physics::left_corner_stacking(buffer, x, y, SAND_COLOR)
-                        }
-                        Position::Middle => physics::middle_stacking(buffer, x, y, SAND_COLOR),
+                    SAND_COLOR => match check_for_cases(&Screen, x, y) {
+                        Position::RightSide => physics::right_corner_stacking(Screen, x, y),
+                        Position::LeftSide => physics::left_corner_stacking(Screen, x, y),
+                        Position::Middle => physics::middle_stacking(Screen, x, y),
                     },
                     _ => (),
                 }
@@ -92,10 +101,10 @@ fn update_physics(buffer: &mut Vec<u32>) {
     }
 }
 
-fn check_for_cases(x: usize, y: usize) -> Position {
+fn check_for_cases(Screen: &Screen, x: usize, y: usize) -> Position {
     if x == 0 {
         Position::RightSide
-    } else if x == WIDTH {
+    } else if x == Screen.WIDTH {
         Position::LeftSide
     } else {
         Position::Middle
--- src/physics.rs
+++ src/physics.rs
@@ -1,16 +1,16 @@
-pub fn gravity(buffer: &mut Vec<u32>, x: usize, y: usize, SAND_COLOR: usize) {
-    buffer[x + (y + 1) * WIDTH] = SAND_COLOR;
-    buffer[x + y * WIDTH] = BACKGROUND_COLOR;
+pub fn gravity(Screen: &mut Screen, x: usize, y: usize) {
+    Screen.buffer[x + (y + 1) * Screen.WIDTH] = Particle::Sand.get_color();
+    Screen.buffer[x + y * Screen.WIDTH] = Particle::Background.get_color();
 }
 
-pub fn middle_stacking(buffer: &mut Vec<u32>, x: usize, y: usize) {
-    if buffer[(x + 1) + (y + 1) * WIDTH] == BACKGROUND_COLOR {
-        buffer[(x + 1) + y * WIDTH] = SAND_COLOR;
-        buffer[x + y * WIDTH] = BACKGROUND_COLOR;
-    } else if buffer[(x - 1) + (y + 1) * WIDTH] == BACKGROUND_COLOR {
-        buffer[(x - 1) + y * WIDTH] = SAND_COLOR;
-        buffer[x + y * WIDTH] = BACKGROUND_COLOR;
+pub fn middle_stacking(Screen: &mut Screen, x: usize, y: usize) {
+    if Screen.buffer[(x + 1) + (y + 1) * Screen.WIDTH] == Particle::Background.get_color() {
+        Screen.buffer[(x + 1) + y * Screen.WIDTH] = Particle::Sand.get_color();
+        Screen.buffer[x + y * Screen.WIDTH] = Particle::Background.get_color();
+    } else if Screen.buffer[(x - 1) + (y + 1) * Screen.WIDTH] == Particle::Background.get_color() {
+        Screen.buffer[(x - 1) + y * Screen.WIDTH] = Particle::Sand.get_color();
+        Screen.buffer[x + y * Screen.WIDTH] = Particle::Background.get_color();
     }
 }
-pub fn right_corner_stacking(buffer: &mut Vec<u32>, x: usize, y: usize) {}
-pub fn left_corner_stacking(buffer: &mut Vec<u32>, x: usize, y: usize) {}
+pub fn right_corner_stacking(Screen: &mut Screen, x: usize, y: usize) {}
+pub fn left_corner_stacking(Screen: &mut Screen, x: usize, y: usize) {}
